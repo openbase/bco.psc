f6910a004fbef1515b17a1046935e256ae225b5e
Thoren Huppke <thuppke@users.noreply.github.com>
2017-09-25 17:07:20 +0200

    Added some output

--- control/src/main/java/org/openbase/bco/psc/control/ControlController.java
+++ control/src/main/java/org/openbase/bco/psc/control/ControlController.java
@@ -1,6 +1,7 @@
 package org.openbase.bco.psc.control;
 
-/*-
+/*
+ * -
  * #%L
  * BCO PSC Control
  * %%
@@ -13,11 +14,11 @@ package org.openbase.bco.psc.control;
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
@@ -101,7 +102,9 @@ public class ControlController extends AbstractEventHandler implements Control, 
 
                 controllableObjectRegistry = new SynchronizableRegistryImpl<>();
                 registryFlags = JPService.getProperty(JPPscUnitFilterList.class).getValue();
+                LOGGER.info("Selected Control Registry flags: " + registryFlags.toString());
                 threshold = JPService.getProperty(JPControlThreshold.class).getValue();
+                LOGGER.info("Selected Control threshold: " + threshold);
 
                 initializeRegistryConnection();
 
