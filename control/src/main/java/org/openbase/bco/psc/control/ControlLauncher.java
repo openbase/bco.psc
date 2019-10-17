233ddc308aa62a6be3c07076142f95c76d9050f4
Dennis Reinsch <49169493+dreinsch@users.noreply.github.com>
2019-10-17 15:06:46 +0200

    Add authors

--- control/src/main/java/org/openbase/bco/psc/control/ControlLauncher.java
+++ control/src/main/java/org/openbase/bco/psc/control/ControlLauncher.java
@@ -39,6 +39,9 @@ import org.openbase.jul.pattern.launch.AbstractLauncher;
  * The launcher of this application.
  *
  * @author <a href="mailto:thuppke@techfak.uni-bielefeld.de">Thoren Huppke</a>
+ * @author <a href="mailto:dreinsch@techfak.uni-bielefeld.de">Dennis Reinsch</a>
+ * @author <a href="mailto:jbitschene@techfak.uni-bielefeld.de">Jennifer Bitschene</a>
+ * @author <a href="mailto:jniermann@techfak.uni-bielefeld.de">Julia Niermann</a>
  */
 public class ControlLauncher extends AbstractLauncher<ControlController> {
 
