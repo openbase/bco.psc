c353114f073bff27b8258b441b5c2d185a15243b
Dennis Reinsch <49169493+dreinsch@users.noreply.github.com>
2019-10-17 15:10:44 +0200

    Add authors

--- control/src/main/java/org/openbase/bco/psc/control/jp/JPMultimodalMode.java
+++ control/src/main/java/org/openbase/bco/psc/control/jp/JPMultimodalMode.java
@@ -2,7 +2,13 @@ package org.openbase.bco.psc.control.jp;
 
 import org.openbase.jps.exception.JPNotAvailableException;
 import org.openbase.jps.preset.AbstractJPBoolean;
-
+/**
+ * JavaProperty used to enable the multimodal mode.
+ *
+ * @author <a href="mailto:dreinsch@techfak.uni-bielefeld.de">Dennis Reinsch</a>
+ * @author <a href="mailto:jbitschene@techfak.uni-bielefeld.de">Jennifer Bitschene</a>
+ * @author <a href="mailto:jniermann@techfak.uni-bielefeld.de">Julia Niermann</a>
+ */
 public class JPMultimodalMode extends AbstractJPBoolean {
     /**
      * The identifiers that can be used in front of the command line argument.
