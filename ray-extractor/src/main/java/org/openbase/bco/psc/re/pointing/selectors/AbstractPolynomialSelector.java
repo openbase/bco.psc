4166ce6d0d9e5c7be261ea9dd6817a24b2b21604
Divine Threepwood <divine@openbase.org>
2020-01-02 22:04:42 +0100

    update year in license header.

--- ray-extractor/src/main/java/org/openbase/bco/psc/re/pointing/selectors/AbstractPolynomialSelector.java
+++ ray-extractor/src/main/java/org/openbase/bco/psc/re/pointing/selectors/AbstractPolynomialSelector.java
@@ -5,7 +5,7 @@ package org.openbase.bco.psc.re.pointing.selectors;
  * #%L
  * BCO PSC Ray Extractor
  * %%
- * Copyright (C) 2016 - 2019 openbase.org
+ * Copyright (C) 2016 - 2020 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
